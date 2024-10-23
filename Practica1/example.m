f3fbf9b7272d1e49f5bed7d6040633264c0e793e
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-10-22 20:45:37 -0600

    Pactica 1 Interfaz

---
+++ Practica1/example.m
@@ -0,0 +1,29 @@
+clc;
+clear;
+[audio, Fs] = audioread('perros2.wav'); 
+N = length(audio);  % Número de muestras
+t = (0:N-1)/Fs;  % Vector de tiempo para graficar la señal en segundos
+
+figure;
+subplot(2,1,1); 
+plot(t, audio);
+title('Señal de Audio en el Tiempo');
+xlabel('Tiempo [s]');
+ylabel('Amplitud');
+
+% FFT
+fourier = fft(audio);  % Transformada de Fourier
+frecuencias = (0:floor(N/2)-1)*(Fs/N);  % Vector de frecuencias para la mitad positiva
+
+% Graficar la FFT
+subplot(2,1,2);  
+plot(frecuencias, abs(fourier / N), 'k', 'LineWidth', 1.5);
+title('Transformada de Fourier del Audio');
+xlabel('Frecuencia [Hz]');
+ylabel('Magnitud');
+
+% Encontrar la frecuencia fundamental
+[magnitud_max, idx] = max(abs(fourier));  % Encuentra el pico máximo
+frecuencia_fundamental = frecuencias(idx);  % Obtiene la frecuencia correspondiente
+
+disp(['La frecuencia fundamental es: ', num2str(frecuencia_fundamental), ' Hz']);
