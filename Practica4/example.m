1103d4e40bf7e25094f4f2a39b7c44e540cfe737
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-11-08 00:01:43 -0600

    interfaz practica 4

--- Practica4/example.m
+++ Practica4/example.m
@@ -1,9 +1,12 @@
 clc;
 clear;
-[audio, Fs] = audioread('perros2.wav'); 
-N = length(audio);  % Número de muestras
-t = (0:N-1)/Fs;  % Vector de tiempo para graficar la señal en segundos
 
+% Cargar el archivo de audio
+[audio, Fs] = audioread('perros3.wav'); 
+N = length(audio);         % Número de muestras
+t = (0:N-1) / Fs;          % Vector de tiempo para graficar la señal en segundos
+
+% Graficar la señal de audio en el dominio del tiempo
 figure;
 subplot(2,1,1); 
 plot(t, audio);
@@ -11,39 +14,18 @@ title('Señal de Audio en el Tiempo');
 xlabel('Tiempo [s]');
 ylabel('Amplitud');
 
-%%
+% Aplicar la Transformada de Fourier al audio
+Y = fft(audio);
+P2 = abs(Y / N);           % Espectro de amplitud de dos lados
+P1 = P2(1:N/2+1);          % Convertir a espectro de un solo lado
+P1(2:end-1) = 2 * P1(2:end-1);  % Escalar las amplitudes
 
-X = 2*randn(size(Fs)) + audio;
+% Definir el dominio de frecuencia para el espectro de un solo lado
+f = Fs * (0:(N/2)) / N;
 
-plot(1000*t,X)
-title("Signal Corrupted with Zero-Mean Random Noise")
-xlabel("t (milliseconds)")
-ylabel("X(t)")
-
-%% 
-
-
-
-figure;
-subplot(2,1,1); 
-plot(t, audio);
-title('Señal de Audio en el Tiempo');
-xlabel('Tiempo [s]');
-ylabel('Amplitud');
-
-% FFT
-fourier = fft(audio);  % Transformada de Fourier
-frecuencias = (0:floor(N/2)-1)*(Fs/N);  % Vector de frecuencias para la mitad positiva
-
-% Graficar la FFT
-subplot(2,1,2);  
-plot(frecuencias, abs(fourier / N), 'k', 'LineWidth', 1.5);
-title('Transformada de Fourier del Audio');
+% Graficar el espectro de amplitud de un solo lado
+subplot(2,1,2);
+plot(f, P1, 'LineWidth', 2);
+title('Espectro de Amplitud de un Solo Lado del Audio');
 xlabel('Frecuencia [Hz]');
-ylabel('Magnitud');
-
-% Encontrar la frecuencia fundamental
-[magnitud_max, idx] = max(abs(fourier));  % Encuentra el pico máximo
-frecuencia_fundamental = frecuencias(idx);  % Obtiene la frecuencia correspondiente
-
-disp(['La frecuencia fundamental es: ', num2str(frecuencia_fundamental), ' Hz']);
+ylabel('|P1(f)|');
