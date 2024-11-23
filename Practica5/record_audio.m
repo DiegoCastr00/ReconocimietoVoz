bff7fbe3f2dd507d6af8fdeccb00113e13f5dfe5
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-11-22 19:20:23 -0600

    Audios Diego

---
+++ Practica5/record_audio.m
@@ -0,0 +1,31 @@
+outputFolder = 'Diego'; 
+if ~exist(outputFolder, 'dir')
+    mkdir(outputFolder);
+end
+
+duration = 1;
+fs = 44100;
+
+names = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"];
+
+recorder = audiorecorder(fs, 16, 1);
+
+for i = 1:10
+    fprintf('Preparando para grabar audio %d: "%s"\n', i, names(i));
+    
+    for count = 1:3
+        fprintf('%d\n', count);
+        pause(1); 
+    end
+    
+    fprintf('Grabando audio %d...\n', i);
+    recordblocking(recorder, duration);
+    audioData = getaudiodata(recorder);
+
+    outputFileName = fullfile(outputFolder, sprintf('%d.wav', i));
+    audiowrite(outputFileName, audioData, fs);
+    
+    fprintf('Audio %d guardado como "%d.wav"\n', i, i);
+end
+
+fprintf('Todos los audios han sido grabados y guardados en la carpeta "%s".\n', outputFolder);
