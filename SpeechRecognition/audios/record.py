cc36028106087a9718f7c915a5c1971f89e7660f
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2025-01-07 13:57:12 -0600

    luz

--- SpeechRecognition/audios/record.py
+++ SpeechRecognition/audios/record.py
@@ -6,10 +6,10 @@ from scipy.io.wavfile import write
 
 DURATION = 1
 SAMPLE_RATE = 44100  
-NUM_RECORDINGS = 10
+NUM_RECORDINGS = 50
 
-OUTPUT_FOLDER = "frio"  # Carpeta de salida
-FILE_NAME = f"Nancy_{OUTPUT_FOLDER}"  # Nombre del archivo
+OUTPUT_FOLDER = "luz"  # Carpeta de salida
+FILE_NAME = f"Mike_{OUTPUT_FOLDER}"  # Nombre del archivo
 
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
@@ -21,7 +21,7 @@ for i in range(3):
 print("Comenzando grabaciones...")
     
 # Grabar audios
-for i in range(4, NUM_RECORDINGS + 1):
+for i in range(0, NUM_RECORDINGS + 1):
     print(f"Grabando audio {i}/{NUM_RECORDINGS}...")
     audio_data = sd.rec(int(DURATION * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1, dtype='int16')
     sd.wait() 
